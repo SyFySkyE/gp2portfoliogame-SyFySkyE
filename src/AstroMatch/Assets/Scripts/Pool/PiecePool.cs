dba754bdfddf747ba3f56f12f17653949ce2c2d1
Chris Grych <ceggrych@gmail.com>
2020-04-04 21:04:57 -0500

    Starts pooling system. Need that and command, poc score system and timer

---
+++ src/AstroMatch/Assets/Scripts/Pool/PiecePool.cs
@@ -0,0 +1,85 @@
+﻿/*
+ * Based off of David Crook's Object Pooling From https://blogs.msdn.microsoft.com/dave_crooks_dev_blog/2014/07/21/object-pooling-for-unity3d/
+ */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiecePool 
+{
+    // Collection of stored Pieces.
+    private List<Piece> pooledPieces;
+
+    // Collection of game pieces
+    private Piece[] samplePieceArray;
+
+    // Samples of pieces
+    private IcePiece icePiece;
+    private WaterPiece waterPiece;
+    private SandPiece sandPiece;
+    private PurplePiece purplePiece;
+    private RedPiece redPiece;
+
+    // Pool Size Parameters
+    private int initialPoolSize;
+    private int maxPoolSize;
+
+    public PiecePool(int initialPoolSize, int maxPoolSize)
+    {
+        samplePieceArray = GetPieceArray();
+        pooledPieces = new List<Piece>();
+
+        for (int i = 0; i < initialPoolSize; i++)
+        {
+            Debug.Log("Instantiated Piece");
+            Piece newPiece = GameObject.Instantiate(samplePieceArray[Random.Range(0, samplePieceArray.Length)], Vector3.zero, Quaternion.identity);
+
+            newPiece.gameObject.SetActive(true);
+            pooledPieces.Add(newPiece);
+
+            GameObject.DontDestroyOnLoad(newPiece);
+        }
+
+        this.initialPoolSize = initialPoolSize;
+        this.maxPoolSize = maxPoolSize;        
+    }
+
+    public Piece GetObject()
+    {
+        for (int i = 0; i < pooledPieces.Count; i++)
+        {
+            if (pooledPieces[i].gameObject.activeSelf == false)
+            {
+                pooledPieces[i].gameObject.SetActive(true);
+                return pooledPieces[i];
+            }
+            else
+            {
+                // If we didn't make it this far, there isn't an inactive object in the pool.
+                if (this.maxPoolSize > this.pooledPieces.Count)
+                {
+                    Debug.Log("Instantiated Piece, pool too small");
+                    Piece newPiece = GameObject.Instantiate(samplePieceArray[Random.Range(0, samplePieceArray.Length)], Vector3.zero, Quaternion.identity);
+                    newPiece.gameObject.SetActive(true);
+                    pooledPieces.Add(newPiece);
+                    return newPiece;
+                }
+            }
+        }
+        Debug.LogError("Cannot retrieve object from pool");
+        return null;
+    }
+
+    private Piece[] GetPieceArray()
+    {
+        Piece[] samplePieces = new Piece[4];
+        samplePieces[0] = icePiece;
+        samplePieces[1] = waterPiece;
+        samplePieces[2] = sandPiece;
+        samplePieces[3] = purplePiece;
+        samplePieces[4] = redPiece;
+        return samplePieces;
+    }
+}
---
+++ src/AstroMatch/Assets/Scripts/Pool/PiecePoolingManager.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Based off of David Crook's Object Pooling From https://blogs.msdn.microsoft.com/dave_crooks_dev_blog/2014/07/21/object-pooling-for-unity3d/
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiecePoolingManager 
+{
+    // Volitate to ensure that assignment must be finished before it is accessed
+    private static volatile PiecePoolingManager instance;
+
+    private Dictionary<string, PiecePool> objectPools;
+
+    // Most light-weight object used for locking
+    private static object syncRoot = new System.Object();
+
+    private PiecePoolingManager()
+    {
+        this.objectPools = new Dictionary<string, PiecePool>();
+    }
+
+    public static PiecePoolingManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                lock (syncRoot)
+                {
+                    if (instance == null)
+                    {
+                        instance = new PiecePoolingManager();
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+
+    public Piece PooledObject { get; internal set; }
+
+    public bool CreatePool(Piece pieceToPool, int initialPoolSize, int maxPoolSize)
+    {
+        if (PiecePoolingManager.Instance.objectPools.ContainsKey(pieceToPool.name))
+        {
+            return false;
+        }
+        else
+        {
+            PiecePool nPool = new PiecePool(initialPoolSize, maxPoolSize);
+            PiecePoolingManager.Instance.objectPools.Add(pieceToPool.name, nPool);
+            return true;
+        }
+    }
+
+    public Piece GetPiece(string pieceName)
+    {
+        return PiecePoolingManager.Instance.objectPools[pieceName].GetObject();
+    }
+}
