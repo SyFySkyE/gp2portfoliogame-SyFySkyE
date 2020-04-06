25c5ec101c0b1228192fd4ab610dadd71e7dd0e9
Chris Grych <ceggrych@gmail.com>
2020-04-06 18:13:14 -0500

    Adds sample timer

--- src/AstroMatch/Assets/GameTimer.cs
+++ src/AstroMatch/Assets/GameTimer.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class GameTimer : MonoBehaviour
 {
+    [Header("Initial Time, in seconds")]
+    [SerializeField] private float timer = 30;
+
+    private TMPro.TextMeshProUGUI timeText;
+ 
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeText = GetComponent<TMPro.TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+        timeText.text = timer.ToString("F2");
     }
 }
