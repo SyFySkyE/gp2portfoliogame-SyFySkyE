e34d4ff795154d65e8bb8d6a3007241670ea5b43
Chris Grych <ceggrych@gmail.com>
2020-04-29 21:23:44 -0500

    Adds reconstructed AI

--- src/AstroMatch/Assets/Scripts/Playables/UnityComputerOpponent.cs
+++ src/AstroMatch/Assets/Scripts/Playables/UnityComputerOpponent.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class UnityComputerOpponent : UnityBasePlayer
 {
     [Header("AI Opponent Parameter")]
-    [SerializeField] private float secondsBeforeEnemyMove = 1.5f;
+    [SerializeField] private float secondsBeforeEnemyMove = 2f;
     [SerializeField] private float enemyTimeDecrement = 0.1f;
 
     private AI computerControlledOpponent;
@@ -36,7 +36,7 @@ public class UnityComputerOpponent : UnityBasePlayer
 
     private void MakeAICommand()
     {
-        SinglePiece connectedPiece = computerControlledOpponent.SelectNextPiece();
+        SinglePiece connectedPiece = computerControlledOpponent.AICommand();
         SelectPiece selectPieceCommand = new SelectPiece(GetComponent<UnityGrid>().UnityPieces[(int)connectedPiece.Location.x, (int)connectedPiece.Location.y]);
         commandProcessor.AddNewCommand(selectPieceCommand);
     }
